7b7097b826f7a04ce0b8829ab6c346d68542c05f
Naoto Sato <naoto.sato@oracle.com>
2023-02-13 20:40:10 +0000

    Adapted for linux

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 public class Main {
-    private static final String MODDIR = "/Users/nsato/dev/jdk/git/unicode/open/src/jdk.charsets/";
+    private static final String MODBASE = "/home/nsato/projects/jdk/git/jdk/open/src/java.base/";
+    private static final String MODEXT = "/home/nsato/projects/jdk/git/jdk/open/src/jdk.charsets/";
     public static void main(String[] args) throws Throwable {
         var para = new String[] {
-            "/Users/nsato/dev/jdk/git/unicode/open/make/data/charsetmapping",
+            "/home/nsato/projects/jdk/git/jdk/open/make/data/charsetmapping",
             "out",
-            "extcs",
+            "stdcs",
             "charsets",
             "stdcs-linux",
-            MODDIR + "share/classes/sun/nio/cs/ext/ExtendedCharsets.java.template",
-                MODDIR + "share/classes/sun/nio/cs/ext",
-                "/Users/nsato/dev/jdk/git/unicode/open/make/jdk/src/classes/build/tools/charsetmapping"
+            MODBASE + "share/classes/sun/nio/cs/StandardCharsets.java.template",
+                MODEXT + "share/classes/sun/nio/cs/ext",
+                "/home/nsato/projects/jdk/git/jdk/open/make/jdk/src/classes/build/tools/charsetmapping"
         };
         build.tools.charsetmapping.Main.main(para);
     }
